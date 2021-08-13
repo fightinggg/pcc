153bd3f2b6270c59d16a333901f9894d04cafa80
hellowang <hellowang@tencent.com>
2021-08-13 15:26:42 +0800

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,13 @@
 
-#include<cstdio>
-#include <iostream>
-#include "nfa.hpp"
-#include "regex_lite.hpp"
-#include "parsing.hpp"
+#include<iostream>
 
 using namespace std;
 
-string code = "\n"
-              "#include<cstdio>\n"
-              "#include <iostream>\n"
-              "#include \"nfa.hpp\"\n"
-              "#include \"regex_lite.hpp\"\n"
-              "#include \"parsing.hpp\"\n"
-              "\n"
-              "using namespace std;\n"
-              "\n"
-              "string code = \"\"\n"
-              "\n"
-              "int main() {\n"
-              "    printf(\"hello pcc\\n\");\n"
-              "    parsing::_dfs_parsing(\"\");\n"
-              "\n"
-              "}";
 
 int main() {
-    printf("hello pcc\n");
-    grammar gr;
-    parsing::dfs_parsing(code, gr);
-
+    int a = 1;
+    int b = 2;
+    int c = a + b * a;
+    cout << c << endl;
 }
+
